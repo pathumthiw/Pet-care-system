7d41b79b8fa8bb41642ae553512ed2bf2de03fc6
pathumthiw <pathumthivanka12asd@gmail.com>
2024-09-28 15:02:43 +0530

    up

---
+++ footer.php
@@ -0,0 +1,67 @@
+<footer>
+        <div class="footer-logo">
+            <img src="../images/logo.jpg">
+        </div>
+        <nav class="footer-nav">
+            <a href="#">FAQ</a><br> 
+            <a href="#">Contact Us</a><br>
+            <a href="#">About Us</a> 
+             
+        </nav>
+        <div class="social-media">
+            <a href="#"><i class="ri-facebook-fill"></i></a>
+            <a href="#"><i class="ri-linkedin-fill"></i></a>
+            <a href="#"><i class="ri-google-fill"></i></a>
+        </div>
+        <p>Copyright © 2024 Website. All rights reserved.</p>
+        <p><a href="#"id="link-a">Terms & Conditions</a> | <a href="#"id="link-a">Privacy Policy</a></p>
+    </footer>
+
+    <style>
+        footer {
+    background-color: #444;
+    color: #fff;
+    text-align: center;
+    padding: 2rem 0;
+}
+
+footer .footer-logo img{
+    font-size: 1.5rem;
+    margin-bottom: 1rem;
+    width: 120px;
+    height: 75px;
+}
+
+footer nav {
+    margin-bottom: 1rem;
+}
+
+footer nav a {
+    color: #fff;
+    text-decoration: none;
+    margin: 0 0.5rem;
+    row-gap: 20px;
+}
+footer nav a {
+    color: #fff;
+    text-decoration: none;
+    margin: 0 0.5rem;
+}
+
+footer .social-media a {
+    color: #fff;
+    margin: 0 1rem;
+    text-decoration: none;
+}
+
+footer p {
+    margin-top: 1rem;
+    font-size: 0.8rem;
+}
+#link-a{
+    text-decoration: none;
+}
+#link-a:visited{
+    color:#fff;
+}
+</style>
